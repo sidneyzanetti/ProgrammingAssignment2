4fd2628be0f6912f5ab6ccc7a4043e9a383c3b38
Sidney Zanetti <sidneyzanetti@Sidneys-MacBook-Pro.local>
2014-10-24 17:56:03 -0200

    assignment resolved

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+# Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather than 
+# computing it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). Your assignment is to 
+# write a pair of functions that cache the inverse of a matrix.
 
-## Write a short comment describing this function
+## HOW TO USE:
+# > z = rbind(c(1, -1/4), c(-1/4, 1))
+# > x <- makeCacheMatrix()
+# > x$set(z)
+# > cacheSolve(x)
+# [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
+# > cacheSolve(x)
+# getting cached data
+# [,1]      [,2]
+# [1,] 1.0666667 0.2666667
+# [2,] 0.2666667 1.0666667
+#
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## This function creates a special "matrix" object that can cache its inverse.
+ <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+## If the inverse has already been calculated (and the matrix has not changed), then cacheSolve should 
+## retrieve the inverse from the cache.
+## PS: This funciton assumes that the matrix supplied is always invertible.
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m  
 }
